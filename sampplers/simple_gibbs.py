5d305dd5df36581b854f689c479beda7ab385f4a
kacper chwialkowski <kacper.chwialkowski@gmail.com>
2015-12-18 17:13:03 +0100

    haha

--- sampplers/simple_gibbs.py
+++ sampplers/simple_gibbs.py
@@ -1,29 +1,64 @@
 import random
 import math
-import numpy as np
+import autograd.numpy as np
 from autograd import grad
-__author__ = 'kcx'
-
-def gibbs(N=50000,thin=10):
-    x=0
-    y=0
-    xvex = np.zeros(N)
-    yvec  = np.zeros(N)
-
-    for i in range(N):
-        for j in range(thin):
-            x=random.gammavariate(3,1.0/(y*y+4))
-            y=random.gauss(1.0/(x+1),1.0/math.sqrt(2*x+2))
-        xvex[i] = x
-        yvec[i] = y
-    return xvex,yvec
 
 
+def log_ugly(x):
+    return -(x/10 + np.sin(x) )**2/2
 
-def almost_density(x,y,k=10):
-    return k*x**2*np.exp(-x*y**2 - y**2 + 2*y - 4*y)
+
+grad_log_ugly = grad(log_ugly)
+
+def test_function(x,omega=0.4):
+    return np.exp( -(x-omega)**2/2)
+
+grad_test = grad(test_function)
+
+def stat(x):
+    return grad_log_ugly(x)*test_function(x) + grad_test(x)
+
+x_prev = 0.1
 
 
-grad_almost = grad(almost_density)
 
-def g
+
+A = [x_prev]
+N = 100000
+for i in range(N-1):
+    guess = np.random.randn()+x_prev
+    old_log_lik = log_ugly(x_prev)
+    new_log_lik = log_ugly(guess)
+    if new_log_lik > old_log_lik:
+        A.append(guess)
+    else:
+        u = random.uniform(0.0,1.0)
+        if (u < math.exp(new_log_lik - old_log_lik)):
+            A.append(guess)
+        else:
+            A.append(x_prev)
+    x_prev = A[-1]
+
+import matplotlib.pyplot as plt
+
+
+
+assert len(A)==N
+
+
+for i in range(N):
+    A[i] = stat(A[i])
+
+rows = int(N/1000)
+
+A = np.reshape(A,(rows,1000))
+
+imnormal = np.zeros(rows)
+for row in range(rows):
+    imnormal[row] = np.abs((np.sqrt(1000.0) * np.mean(A[row])) / np.var(A[row]))
+
+
+
+plt.plot(imnormal)
+
+plt.show()
