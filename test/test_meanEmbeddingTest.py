4ab36bb0a27021b9f19a5818e040309d41f0bb8d
kacper chwialkowski <kacper.chwialkowski@gmail.com>
2015-12-25 19:10:52 +0100

    it kurwa works

--- test/test_meanEmbeddingTest.py
+++ test/test_meanEmbeddingTest.py
@@ -51,3 +51,18 @@ class TestMeanEmbeddingTest(TestCase):
         data = np.random.randn(10000,4)
         me = GaussianSteinTest(data,grad_log_normal,3)
         assert me.compute_pvalue() > 0.05
+
+
+    def test_on_four_dim_gaus2(self):
+
+        np.random.seed(42)
+        def grad_log_normal(x):
+            return  -x
+
+        data = np.random.randn(10000,4)+0.01*np.random.rand()
+        me = GaussianSteinTest(data,grad_log_normal,10)
+        p1 = me.compute_pvalue()
+        me = GaussianSteinTest(data,grad_log_normal,1)
+        p2 = me.compute_pvalue()
+        print(p1,p2)
+        assert p1 <p2
