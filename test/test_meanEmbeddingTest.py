88fcf6851b1568be2de134b51674c80ad1342bcc
kacper chwialkowski <kacper.chwialkowski@gmail.com>
2015-12-25 19:08:30 +0100

    works for multiple dimensions

--- test/test_meanEmbeddingTest.py
+++ test/test_meanEmbeddingTest.py
@@ -28,4 +28,26 @@ class TestMeanEmbeddingTest(TestCase):
 
         data = np.random.randn(10000,4)
         me = GaussianSteinTest(data,grad_log_normal,1)
+        assert me.compute_pvalue() > 0.05
+
+    def test_on_one_dim_gaus2(self):
+        np.random.seed(42)
+
+        def grad_log_normal(x):
+            return  -x
+
+
+        data = np.random.randn(10000)
+        me = GaussianSteinTest(data,grad_log_normal,3)
         assert me.compute_pvalue()>0.05
+
+
+    def test_on_four_dim_gaus2(self):
+
+        np.random.seed(42)
+        def grad_log_normal(x):
+            return  -x
+
+        data = np.random.randn(10000,4)
+        me = GaussianSteinTest(data,grad_log_normal,3)
+        assert me.compute_pvalue() > 0.05
