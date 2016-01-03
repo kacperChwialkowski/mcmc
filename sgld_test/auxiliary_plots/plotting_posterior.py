wrong normalization in plotting posterior

sgld_test/auxiliary_plots/plotting_posterior.py
@@ -6,7 +6,7 @@ from sgld_test.test import gen_X, _log_probability
 
 sns.set(color_codes=True)
 
-N = 1000
+N = 500
 X = gen_X(N)
 theta1 = np.arange(-2, 2, 0.25)
 grid_size = len(theta1)
@@ -14,14 +14,17 @@ theta2 = np.arange(-2, 2, 0.25)
 theta1, theta2 = np.meshgrid(theta1, theta2)
 Z = np.copy(theta1)
 
-log_den = np.exp(-(X ** 2 / 24.0)) / (4.0 * np.sqrt(6.0 * np.pi)) + np.exp(-(X ** 2 / 26)) / (2 * np.sqrt(26 * np.pi))
-
-log_den = np.sum(np.log(log_den))
 
 for i in range(grid_size):
     for j in range(grid_size):
         probability = _log_probability(theta1[i, j], theta2[i, j], X)
-        Z[i, j] = np.exp(probability - log_den)
+        Z[i, j] = probability
+
+max = np.max(Z)+2
+
+Z = np.exp(Z -max)
+
+print(Z)
 
 plt.figure()
 CS = plt.contour(theta1, theta2, Z, 10)

sgld_test/test.py
@@ -22,8 +22,7 @@ def _log_lik(theta_1, theta_2, x):
 def _log_probability(theta_1,theta_2,x):
     log_lik = _log_lik(theta_1, theta_2, x)
 
-    log_prior = np.log(norm.pdf(theta_1,0, SIGMA_1) + norm.pdf(theta_2,0, SIGMA_2))
-
+    log_prior = np.log(norm.pdf(theta_1,0, SIGMA_1)) + np.log(norm.pdf(theta_2,0, SIGMA_2))
 
     return log_lik+log_prior
 #