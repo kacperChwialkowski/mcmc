df203239cd51b222689ed8d546c606635582ffd9
kacper chwialkowski <kacper.chwialkowski@gmail.com>
2016-01-03 11:40:22 +0100

    moving manual gradient to another file

--- sgld_test/plotting_mcmc_sample.py
+++ sgld_test/plotting_mcmc_sample.py
@@ -15,15 +15,32 @@ X = gen_X(400)
 def vectorized_log_density(theta):
      return log_probability(theta,X)
 
-sample = []
-for i in range(400):
-    print(i)
-    sample.append(metropolis_hastings(vectorized_log_density, chain_size=400, thinning=1, x_prev=np.random.randn(2))[-1])
+def grad_log_pob(theta):
+    s=[]
+    for t in theta:
+        s.append( np.sum(manual_grad(t[0],t[1],X),axis=0))
+    return np.array(s)
 
-sample = np.array(sample)
+me = GaussianSteinTest(grad_log_pob,1)
+
+
+pvals = []
+for size in range(10,400,10):
+    sample = []
+    print(size)
+    for i in range(400):
+        sample.append(metropolis_hastings(vectorized_log_density, chain_size=size, thinning=1, x_prev=np.random.randn(2))[-1])
+
+    sample = np.array(sample)
+    pvals.append(me.compute_pvalue(sample))
+
+import matplotlib.pyplot as plt
+
+plt.plot(pvals)
+plt.show()
 
 # #
-print(acf(sample[:,1],nlags=20))
+# print(acf(sample[:,1],nlags=20))
 
 with sns.axes_style("white"):
      sns.jointplot(x=sample[:,1], y=sample[:,0],kind='kde', color="k");
@@ -31,11 +48,6 @@ with sns.axes_style("white"):
 
 
 
-def grad_log_pob(theta):
-    s=[]
-    for t in theta:
-        s.append( np.sum(manual_grad(t[0],t[1],X),axis=0))
-    return np.array(s)
 
 me = GaussianSteinTest(grad_log_pob,1)
 
