abeaf658d72f43c8c884a855dee41cd7027c859a
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-01-10 13:36:00 +0000

    added vectorised 1d statistic, including helper functions and unit tests

--- stat_test/quadratic_time.py
+++ stat_test/quadratic_time.py
@@ -1,28 +1,81 @@
-import warnings
-import numpy as np
-from numpy.linalg import linalg, LinAlgError
-from scipy.stats import chi2
+from scipy.spatial.distance import squareform, pdist
 
-__author__ = 'kcx'
+import numpy as np
+
+
+__author__ = 'kcx, heiko'
 
 
 class GaussianQuadraticTest:
-    def __init__(self, grad_log_prob, scaling=2.0):
+    def __init__(self, grad_log_prob, scaling=2.0, grad_log_prob_multiple=None):
         self.scaling = scaling
         self.grad = grad_log_prob
+        
+        # construct (slow) multiple gradient handle if efficient one is not given
+        if grad_log_prob_multiple is None:
+            def grad_multiple(X):
+                # simply loop over grad calls. Slow
+                return np.array([self.grad(x) for x in X])
+            
+            self.grad_multiple = grad_multiple
+        else:
+            self.grad_multiple = grad_log_prob_multiple
+            
+    def k(self, x, y):
+        return np.exp(-(x - y) ** 2 / self.scaling)
+    
+    def k_multiple(self, X):
+        """
+        Efficient computation of kernel matrix without loops
+        
+        Effectively does the same as calling self.k on all pairs of the input
+        """
+        assert(X.ndim == 1)
+        
+        sq_dists = squareform(pdist(X.reshape(len(X), 1), 'sqeuclidean'))
+            
+        K = np.exp(-(sq_dists) / self.scaling)
+        return K
 
-    def k(self,x,y):
-        return np.exp(-(x-y)**2/self.scaling)
+    def g1k(self, x, y):
+        return -2.0 / self.scaling * self.k(x, y) * (x - y)
+    
+    def g1k_multiple(self, X):
+        """
+        Efficient gradient computation of Gaussian kernel with multiple inputs
+        
+        Effectively does the same as calling self.g1k on all pairs of the input
+        """
+        assert X.ndim == 1
+        
+        differences = X.reshape(len(X), 1) - X.reshape(1, len(X))
+        sq_differences = differences ** 2
+        K = np.exp(-sq_differences / self.scaling)
 
-    def g1k(self,x,y):
-        return -2.0/self.scaling *self.k(x,y)*(x-y)
+        return -2.0 / self.scaling * K * differences
 
-    def g2k(self,x,y):
-        return -self.g1k(x,y)
+    def g2k(self, x, y):
+        return -self.g1k(x, y)
+    
+    def g2k_multiple(self, X):
+        return -self.g1k_multiple(X)
 
-    def gk(self,x,y):
-        return 2.0 *self.k(x,y) *( self.scaling - 2*(x-y)**2)/self.scaling**2
+    def gk(self, x, y):
+        return 2.0 * self.k(x, y) * (self.scaling - 2 * (x - y) ** 2) / self.scaling ** 2
 
+    def gk_multiple(self, X):
+        """
+        Efficient 2nd gradient computation of Gaussian kernel with multiple inputs
+        
+        Effectively does the same as calling self.g2k on all pairs of the input
+        """
+        assert X.ndim == 1
+        
+        differences = X.reshape(len(X), 1) - X.reshape(1, len(X))
+        sq_differences = differences ** 2
+        K = np.exp(-sq_differences / self.scaling)
+
+        return 2.0 * K * (self.scaling - 2 * sq_differences) / self.scaling ** 2
 
     def get_statisitc(self, N, samples):
         U_matrix = np.zeros((N, N))
@@ -37,8 +90,45 @@ class GaussianQuadraticTest:
                 U_matrix[i, j] = a + b + c + d
         stat = N * np.mean(U_matrix)
         return U_matrix, stat
+    
+    def get_statistic_multiple_naive_linear_loop(self, samples):
+        log_pdf_gradients = self.grad_multiple(samples)
+        K = self.k_multiple(samples)
+        G1K = self.g1k_multiple(samples)
+        G2K = self.g2k_multiple(samples)
+        GK = self.gk_multiple(samples)
+        
+        pairwise_log_gradients = log_pdf_gradients.reshape(len(log_pdf_gradients), 1) * log_pdf_gradients.reshape(1, len(log_pdf_gradients))
+        A = pairwise_log_gradients * K
+        B = G1K
+        C = G2K
+        for i in range(len(samples)):
+            B[:, i] *= log_pdf_gradients[i]
+            C[i, :] *= log_pdf_gradients[i]
+            
+        D = GK
+        U = A + B + C + D
+        stat = len(samples) * np.mean(U) 
+        return U, stat
 
-    def compute_pvalue(self, samples,boots=100):
+    def get_statistic_multiple(self, samples):
+        log_pdf_gradients = self.grad_multiple(samples)
+        K = self.k_multiple(samples)
+        G1K = self.g1k_multiple(samples)
+        G2K = self.g2k_multiple(samples)
+        GK = self.gk_multiple(samples)
+        
+        # use broadcasting to mimic the element wise looped call
+        pairwise_log_gradients = log_pdf_gradients.reshape(len(log_pdf_gradients), 1) * log_pdf_gradients.reshape(1, len(log_pdf_gradients))
+        A = pairwise_log_gradients * K
+        B = G1K * log_pdf_gradients
+        C = (G2K.T * log_pdf_gradients).T
+        D = GK
+        U = A + B + C + D
+        stat = len(samples) * np.mean(U) 
+        return U, stat
+
+    def compute_pvalue(self, samples, boots=100):
 
         N = samples.shape[0]
         U_matrix, stat = self.get_statisitc(N, samples)
@@ -48,8 +138,8 @@ class GaussianQuadraticTest:
 
         for proc in range(100):
             W = np.sign(np.random.randn(N))
-            WW = np.outer(W,W)
-            st = np.mean(U_matrix*WW)
-            bootsraped_stats[proc] = N*st
+            WW = np.outer(W, W)
+            st = np.mean(U_matrix * WW)
+            bootsraped_stats[proc] = N * st
 
-        return  float(np.sum(bootsraped_stats > stat))/boots
+        return  float(np.sum(bootsraped_stats > stat)) / boots
