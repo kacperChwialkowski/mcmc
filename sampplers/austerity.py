2c9294c76f801da263344471fe82f582ec6b649f
kacper chwialkowski <kacper.chwialkowski@gmail.com>
2016-01-15 18:07:50 +0100

    first implementation not working obviously

---
+++ sampplers/austerity.py
@@ -0,0 +1,47 @@
+from scipy.stats import t
+
+__author__ = 'kcx'
+
+
+import numpy as np
+
+def austerity(log_lik,log_density_prior, X,epsilon,batch_size=30,chain_size=10000, thinning=15, theta_t=np.random.randn()):
+    A = [theta_t]
+
+    dimension=1
+    if hasattr(theta_t, "__len__"):
+        dimension = len(theta_t)
+
+
+    for i in range(chain_size*thinning-1):
+        theta_prime = np.random.randn(dimension)+theta_t
+        u = np.random.randn()
+        mu_0 = np.log(u)+log_density_prior(theta_prime) -log_density_prior(theta_t)
+        if approximate_MH_accept(mu_0,log_lik,X,batch_size,epsilon,theta_prime, theta_t):
+           theta_t = theta_prime
+        A.append(theta_t)
+
+    return np.array(A[::thinning])
+
+
+def approximate_MH_accept(mu_0,log_lik,X,batch_size,epsilon,theta_prime, theta_t):
+
+    iteration_number=0
+    N  = X.shape[0]
+    while True:
+        iteration_number +=1
+        n = iteration_number*batch_size
+        n = min(n, N)
+        sub = np.random.choice(X, n)
+        sub = log_lik(sub, theta_prime) - log_lik(sub, theta_t)
+        l_hat  = np.mean(sub)
+        l_2_hat = np.mean(sub**2)
+        s_l = np.sqrt(l_2_hat - l_hat**2*n/(n-1))
+        s = s_l/ np.sqrt(n)*np.sqrt(1 - (n-1)/(N-1))
+        stat = (l_hat- mu_0)/s
+        delta  = 1 - t.sf(stat, n-1)
+        print(delta,epsilon)
+        if delta < epsilon:
+            if l_hat > mu_0:
+                return True
+            return False
