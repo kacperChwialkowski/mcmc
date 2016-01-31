9d07f18a0d7f726474dc1cf363e8230a1f20e9bd
kcx <kacper.chwialkowski@gmail.com>
2016-01-31 22:02:32 +0000

    the miracle of birth

--- sgld_test/heikos_experiment/a.py
+++ sgld_test/heikos_experiment/a.py
@@ -3,47 +3,55 @@ from pandas import DataFrame
 import seaborn as sns
 from tools.latex_plot_init import plt
 
+p_values = np.load('pvals.npy')
 
-p_vals = np.load('pvals.npy')
-evals  = np.load('no_evals.npy')/10**6
-epsilon = np.linspace(0.001, 0.2,25)
-
+likelihood_evaluations = np.load('no_evals.npy') / 10 ** 6
+epsilon = np.linspace(0.001, 0.2, 25)
 epsilon = ["%.2f" % v for v in epsilon]
-epsilon[0]="0.001"
-
-data_pvals = []
-
-for i,r in enumerate(p_vals):
-    for eval in r:
-         data_pvals.append( [epsilon[i], eval])
-
-data_pvals = DataFrame(data_pvals)
-
-data_evals = []
-for i,r in enumerate(evals):
-    for eval in r:
-        data_evals.append([epsilon[i], eval])
+epsilon[0] = "0.001"
 
 
-data_evals = DataFrame(data_evals)
 
+def to_data_frame(arr,epsilon):
+    data = []
+    for i, r in enumerate(arr):
+        for eval in r:
+            data.append([epsilon[i], eval])
+    return DataFrame(data)
 
-f, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 4), sharex=True)
+p_values = to_data_frame(p_values[::5], epsilon[::5])
 
-sns.boxplot(x=0,y=1,data = data_pvals, palette="BuGn_d", ax=ax1)
-ax1.set_ylabel("p values")
-ax1.set_xlabel("")
+# likelihood_evaluations = to_data_frame(likelihood_evaluations, epsilon)
 
-y2 = evals
-sns.barplot(x=0,y=1,data = data_evals,palette="RdBu_r", ax=ax2)
+plt.figure()
+sns.boxplot(x=0, y=1, data=p_values, palette="BuGn_d")
+plt.ylabel("p values")
+plt.xlabel("epsilon")
+plt.tight_layout()
+plt.savefig('../../write_up/img/Heiko1.pdf')
 
-ax2.set_ylabel("Likelihood evaluations")
+plt.figure()
+plt.plot(epsilon[::2],np.mean(likelihood_evaluations[::2],axis=1),'g')
+plt.ylabel("likelihood evaluations")
+plt.xlabel("epsilon")
+plt.tight_layout()
+plt.savefig('../../write_up/img/Heiko2.pdf')
+#
+# f, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 4), sharex=True)
+#
+# sns.boxplot(x=0, y=1, data=p_values, palette="BuGn_d", ax=ax1)
+# ax1.set_ylabel("p values")
+# ax1.set_xlabel("")
+#
+# y2 = likelihood_evaluations
+# sns.barplot(x=0, y=1, data=likelihood_evaluations, palette="RdBu_d", ax=ax2)
+#
+# ax2.set_ylabel("Likelihood evaluations")
+#
+# sns.despine(bottom=True)
+# ax2.set(xlabel='epsilon')
+#
+# f.tight_layout()
+# plt.show()
 
-sns.despine(bottom=True)
-ax2.set(xlabel='epsilon')
-
-
-f.tight_layout()
-plt.show()
-
-f.savefig('../../write_up/img/Heiko.pdf')
+# f.savefig('../../write_up/img/Heiko.pdf')
