58ac13cab536d0e2c5329248eb163883d132ea64
kacper chwialkowski <kacper.chwialkowski@gmail.com>
2017-01-09 21:55:02 -0800

    checking if it works fine

--- stat_test/Baringhaus.py
+++ stat_test/Baringhaus.py
@@ -1,6 +1,7 @@
 
 from stat_test.quadratic_time import GaussianQuadraticTest, QuadraticMultiple2
 
+
 __author__ = 'kcx'
 from scipy.spatial.distance import squareform, pdist
 
@@ -9,11 +10,9 @@ from statsmodels.stats.multitest import multipletests
 from stat_test.ar import simulate, simulatepm
 
 
-# null are only Gaussians with id covariance
-# class BaringhausTest:
-
-
-def stat(samples):
+# null is Gaussian with id covariance, that is the magic constants here are 
+# for the id covariance 
+def baringhaus_stat(samples):
     Y = samples - np.mean(samples)
     n = Y.shape[0]
     d = Y.shape[1]
@@ -28,89 +27,48 @@ def stat(samples):
 
 
 
-if __name__ == "__main__":
-    # samples = []
-    # for i in range(300):
-    #     samples.append(stat(np.random.randn(200,2)))
-    # samples1 = []
-    # for i in range(300):
-    #      samples1.append(stat(np.random.randn(1000,2)))
 
+
+if __name__ == "__main__":
+
+    AVERAGE_OVER = 100
+    BOOTSTRAP_SIZE = 300
     def grad_log_normal( x):
         return -x
 
+    def run_simulation(sample_size):
+
+        for d in [2, 5, 10, 15, 20, 25]:
+            samples = []
+            for i in range(BOOTSTRAP_SIZE):
+                samples.append(baringhaus_stat(np.random.randn(sample_size, d)))
+            samples = np.array(samples)
+            pvals = []
+            pvals2 = []
+            for i in range(AVERAGE_OVER):
+                X = np.random.randn(SAMPLE_SIZE, d)
+                X[:, 0] += np.random.rand(SAMPLE_SIZE)
+
+                T = baringhaus_stat(X)
+                pval = len(samples[samples > T]) / BOOTSTRAP_SIZE
+                pvals.append(pval)
+
+                me = GaussianQuadraticTest(grad_log_normal)
+                qm = QuadraticMultiple2(me)
+
+                p = qm.is_from_null(0.1, X, 0.5)
+                pvals2.append(p)
+
+            print('d :', d)
+            pvals = np.array(pvals)
+            print('baringhaus :', len(pvals[pvals < 0.1]) / AVERAGE_OVER)
+
+            pvals2 = np.array(pvals2)
+            print('Stein  :', len(pvals2[pvals2 < 0.1]) / AVERAGE_OVER)
+
     SAMPLE_SIZE = 500
-    # n = 400
-    sim = []
-    for d in [70]:
-        samples = []
-        for i in range(300):
-            samples.append(stat(np.random.randn(SAMPLE_SIZE,d)))
-        samples = np.array(samples)
-        pvals =[]
-        pvals2 =[]
-        for i in range(100):
-
-            X = np.random.randn(SAMPLE_SIZE,d)
-            X[:,0] += np.random.rand(SAMPLE_SIZE)
-
-            T = stat(X)
-            pval = len(samples[samples>T])/300
-            pvals.append(pval)
-
-            # me = GaussianQuadraticTest(grad_log_normal)
-            # qm = QuadraticMultiple2(me)
-            #
-            # p = qm.is_from_null(0.1, X, 0.5)
-            # pvals2.append(p)
-
-        print('d :',d )
-        pvals = np.array(pvals)
-        print('them :',len(pvals[pvals < 0.1])/100)
-
-        # pvals2 = np.array(pvals2)
-        # print('us :',len(pvals2[pvals2 < 0.1])/100)
-
-        #
-        # import matplotlib.pyplot as plt
-        # plt.plot(sorted(pvals))
-        # plt.show()
-
-
+    run_simulation(SAMPLE_SIZE)
+    print("===")
     SAMPLE_SIZE = 1000
-    # n = 400
-    sim = []
-    for d in [2,5,10,15,20,25,30]:
-        samples = []
-        for i in range(300):
-            samples.append(stat(np.random.randn(SAMPLE_SIZE,d)))
-        samples = np.array(samples)
-        pvals =[]
-        pvals2 =[]
-        for i in range(100):
-
-            X = np.random.randn(SAMPLE_SIZE,d)
-            X[:,0] += np.random.rand(SAMPLE_SIZE)
-
-            T = stat(X)
-            pval = len(samples[samples>T])/300
-            pvals.append(pval)
-
-            me = GaussianQuadraticTest(grad_log_normal)
-            qm = QuadraticMultiple2(me)
-
-            p = qm.is_from_null(0.1, X, 0.5)
-            pvals2.append(p)
-
-        print('d :',d )
-        pvals = np.array(pvals)
-        print('them :',len(pvals[pvals < 0.1])/100)
-
-        pvals2 = np.array(pvals2)
-        print('us :',len(pvals2[pvals2 < 0.1])/100)
-
-        #
-        # import matplotlib.pyplot as plt
-        # plt.plot(sorted(pvals))
-        # plt.show()
+    run_simulation(SAMPLE_SIZE)
 
